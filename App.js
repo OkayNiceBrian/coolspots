746df04e516837f4f237484cf79e1b03bd2ef9d6
Brian <brianjackson1224@gmail.com>
2021-08-24 10:33:45 -0700

    Starting on splash screen

--- App.js
+++ App.js
@@ -7,40 +7,34 @@ export default function App() {
   console.log("App Executing");
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground source={require('./assets/laura-vinck-unsplash.jpg')} style={styles.container}>
-        <View style={styles.login}>
-          <View style={{
-              backgroundColor: "dodgerblue",
-              height: 100,
-              width: 100,
-            }} />
-            <View style={{
-              backgroundColor: "gold",
-              height: 100,
-              width: 100,
-              top: 20,
-            }} />
-            <View style={{
-              backgroundColor: "tomato",
-              height: 100,
-              width: 100,
-            }} />
-        </View>
-      </ImageBackground>
-    </SafeAreaView>
+    <ImageBackground source={require('./assets/laura-vinck-unsplash.jpg')} resizeMode="cover" style={styles.background}>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.container}> 
+        <View style={{
+          color: "#fff",
+          height: 100,
+          width: 100,
+        }} />
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
     flex: 1,
+    backgroundColor: "#fff",
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
   login: {
     flexDirection: "row",
     justifyContent: "center",
-    alignItems: 'baseline',
+    alignItems: "center",
     alignContent: "center",
   },
 });
