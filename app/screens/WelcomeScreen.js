3afd9e05fe05d12753f8f0994f747e2a3167e933
Brian <brianjackson1224@gmail.com>
2021-08-24 17:00:41 -0700

    Login and Register are Touchable now

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,6 +1,6 @@
 //import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ImageBackground, StyleSheet, StatusBar, View, Image, Text } from 'react-native';
+import { ImageBackground, StyleSheet, StatusBar, View, Image, Text, TouchableHighlight } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
@@ -16,9 +16,22 @@ function WelcomeScreen(props) {
                 />
                 <Text style={styles.tagline}>Gifts That Keep On Giving</Text>
             </View>
-            <View style={styles.loginButton} />
-            <View style={styles.registerButton} />
-
+            <TouchableHighlight 
+                style={{width: "100%"}} 
+                onPress={() => console.log("Login Pressed")}
+            >
+                <View style={styles.loginButton}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </View>
+            </TouchableHighlight>
+            <TouchableHighlight 
+                style={{width: "100%"}} 
+                onPress={() => console.log("Register Pressed")}
+            >  
+                <View style={styles.registerButton}>
+                    <Text style={styles.buttonText}>Register</Text>
+                </View>
+            </TouchableHighlight>
         </ImageBackground>
     );
 }
@@ -29,10 +42,16 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         alignItems: 'center',
     },
+    buttonText: {
+        fontSize: 22,
+        color: '#fff',
+    },
     loginButton: {
         width: '100%',
         height: 70,
         backgroundColor: '#fc5c65',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     logo: {
         width: 100,
@@ -47,6 +66,8 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 70,
         backgroundColor: '#4ecdc4',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     tagline: {
         fontSize: 24,
