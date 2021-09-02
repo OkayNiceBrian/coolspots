289f590c610c605b13ce07a940e3b6b4a8c38955
Brian Jackson <brianjackson1224@gmail.com>
2021-09-02 13:53:14 -0700

    Adding custom tab icons, needs debugging

--- app/screens/HomeTab.js
+++ app/screens/HomeTab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Ionicons from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen';
 import HomeStack from './HomeStack';
@@ -10,7 +11,20 @@ const Home = ({ navigation }) => {
     return (
         <Tab.Navigator
             screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    let iconSize = 10;
 
+                    if (route.name === 'HomeTab') {
+                        iconName = focused ? 'home' : 'home-outline';
+                    } else if (route.name === 'MapTab') {
+                        iconName = focused ? 'navigate' : 'navigate-outline';
+                    }
+
+                    return <Ionicons name={iconName} size={iconSize} color={'#000'} />
+                },
+                tabBarActiveTintColor: 'black',
+                tabBarInactiveTintColor: 'gray',
             })} 
         >
             <Tab.Screen 
