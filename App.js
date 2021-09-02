ad01cfbeedbb3d3ff1bf8c40fbb057aeeeb73378
Brian Jackson <brianjackson1224@gmail.com>
2021-09-02 00:45:06 -0700

    Added SpotViewScreen but might not be working

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Home from './app/screens/Home';
+import SpotViewScreen from './app/screens/SpotViewScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -12,20 +13,22 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   console.log("App Executing");
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <SpotViewScreen />;
+
+  // return (
+  //   <NavigationContainer>
+  //     <Stack.Navigator>
+  //       <Stack.Screen 
+  //         name="Welcome"
+  //         component={WelcomeScreen}
+  //         options={{ headerShown: false }}
+  //       />
+  //       <Stack.Screen 
+  //         name="Home"
+  //         component={Home}
+  //         options={{ headerShown: true }} // Change to false for production
+  //       />
+  //     </Stack.Navigator>
+  //   </NavigationContainer>
+  // );
 }
---
+++ app/screens/SpotViewScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+function SpotViewScreen(props) {
+    return (
+        <View styles={styles.container}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+});
+
+export default SpotViewScreen;
