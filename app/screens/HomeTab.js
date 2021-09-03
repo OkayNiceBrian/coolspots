762211989714dd32f9f8738f1e97c4c2f1e4c412
Brian <brianjackson1224@gmail.com>
2021-09-03 09:55:21 -0700

    Got Tab icons to work!

--- app/screens/HomeTab.js
+++ app/screens/HomeTab.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Ionicons from '@expo/vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen';
 import HomeStack from './HomeStack';
@@ -7,13 +7,13 @@ import MapScreen from './MapScreen';
 
 const Tab = createBottomTabNavigator();
 
-const Home = ({ navigation }) => {
+const HomeTab = ({ navigation }) => {
     return (
         <Tab.Navigator
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
                     let iconName;
-                    let iconSize = 10;
+                    let iconSize = 25;
 
                     if (route.name === 'HomeTab') {
                         iconName = focused ? 'home' : 'home-outline';
@@ -21,10 +21,12 @@ const Home = ({ navigation }) => {
                         iconName = focused ? 'navigate' : 'navigate-outline';
                     }
 
-                    return <Ionicons name={iconName} size={iconSize} color={'#000'} />
+                    return <Ionicons name={iconName} size={iconSize} color={'black'} />
                 },
                 tabBarActiveTintColor: 'black',
                 tabBarInactiveTintColor: 'gray',
+                tabBarShowLabel: false,
+                
             })} 
         >
             <Tab.Screen 
@@ -41,4 +43,4 @@ const Home = ({ navigation }) => {
     );
 }
 
-export default Home;
+export default HomeTab;
