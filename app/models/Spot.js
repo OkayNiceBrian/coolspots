8daa1226849f5b8873109902172b699445af5af0
Brian Jackson <brianjackson1224@gmail.com>
2021-09-03 21:12:11 -0700

    Added Spot and User models

---
+++ app/models/Spot.js
@@ -0,0 +1,13 @@
+class Spot {
+    constructor(id, name, description, tags, city, latitude, longitude, visible, userId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.tags = tags;
+        this.city = city;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.visible = visible;
+        this.userId = userId;
+    }
+}
