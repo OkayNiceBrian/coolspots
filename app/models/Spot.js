616f524021897dc543efe6745783d2802abc51a7
Brian Jackson <brianjackson1224@gmail.com>
2021-09-03 23:33:10 -0700

    MapScreen api call to return list of spots, updated gitignore with api

--- app/models/Spot.js
+++ app/models/Spot.js
@@ -1,4 +1,4 @@
-class Spot {
+export class Spot {
     constructor(id, name, description, tags, city, latitude, longitude, visible, userId) {
         this.id = id;
         this.name = name;
--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import BottomBar from '../components/BottomBar';
-import TopBar from '../components/TopBar';
+import Spot from '../models/Spot';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, View, Text } from 'react-native';
+import { apiUrl } from '../../global';
 
 function MapScreen(props) {
-
+    const spotList = fetchAllSpots();
     return (
         <View style={styles.container}>
             <MapView 
@@ -13,9 +13,26 @@ function MapScreen(props) {
                 style={styles.map}
                 showsUserLocation={true}
                 showsMyLocationButton={true} 
-            />
+            >
+            </MapView>
         </View>
     );
+}
+
+async function fetchAllSpots() {
+    const spotsUrl = apiUrl + "/spots";
+    const spotList = [];
+    
+    const data = await fetch(spotsUrl)
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < data._embedded.spots.length; i++) {
+                const spotData = data._embedded.spots[i];
+                spotList[i] = spotData;
+            }
+        });
+    console.log(spotList);
+    return spotList;
 }
 
 const styles = StyleSheet.create({
