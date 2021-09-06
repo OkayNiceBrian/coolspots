f75941feacd968a29f316e842c759b41aab71348
Brian Jackson <brianjackson1224@gmail.com>
2021-09-05 17:16:48 -0700

    Removed SpotMarkerList, got markers to show up on MapScreen from api!!!!

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,10 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 import { apiUrl } from '../../global';
-import { SpotMarkerList } from '../components/SpotMarkerList';
 
 function MapScreen(props) {
+    const [isLoading, setLoading] = useState(true);
+    const [spotList, setData] = useState([]);
+
+    const getSpots = async () => {
+        try {
+            const spotsUrl = apiUrl + "/spots";
+            const response = await fetch(spotsUrl);
+            const json = await response.json();
+            setData(json._embedded.spots);
+        } catch (err) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getSpots();
+    }, []);
+
+    const spotMarkers = () => {
+        return spotList.map((spot, index) => (
+            <Marker 
+                key={index}
+                coordinate={{ latitude: spot.latitude, longitude: spot.longitude }}
+                title={spot.name}
+                description={spot.description}
+            />
+        ))
+    }
+
     return (
         <View style={styles.container}>
             <MapView 
@@ -13,45 +43,13 @@ function MapScreen(props) {
                 showsUserLocation={true}
                 showsMyLocationButton={true} 
             >
+                { spotMarkers() }
             </MapView>
         </View>
     );
 }
 
-async function fetchAllSpotsAndReturnMarkers() {
-    const spotsUrl = apiUrl + "/spots";
-    const spotList = [];
-    
-    const data = await fetch(spotsUrl)
-        .then(response => response.json())
-        .then(data => {
-            for (let i = 0; i < data._embedded.spots.length; i++) {
-                const spotData = data._embedded.spots[i];
-                spotList[i] = spotData;
-            }
-        });
-    const markerList = createMarkerList(spotList);
-    return markerList;
-}
 
-function createMarkerList(spotList) {
-    let markerList = [];
-    for (let i = 0; i < spotList.length; i++) {
-        let latlng = {
-            latitude: spotList[i].latitude,
-            longitude: spotList[i].longitude,
-        };
-        markerList.push(
-            <Marker 
-                key={spotList[i].id}
-                coordinate={latlng}
-                title={spotList[i].name}
-                description={spotList[i].description}
-            />
-        );
-    }
-    return markerList;
-} 
 
 const styles = StyleSheet.create({
     container: {
